7431c9aa346123aff1f0d7f595252694c9e6587a
Jonathan Lima <jilds@outlook.com>
2019-01-27 12:14:55 -0300

    Melhorias visuais e multiselect funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -41,6 +42,7 @@ import { AparelhoService } from './services/aparelho.service';
     BrowserModule,
     HttpModule,
     FormsModule,
+    BrowserAnimationsModule,
     routing
   ],
   providers: [ConfigService, UsuarioService, PerfilService, AparelhoService],
